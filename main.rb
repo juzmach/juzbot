8d1e67eac1cae467b6632a5e899aa61e08d305c1
Juuso Strömberg <juuso.stromberg@cs.helsinki.fi>
2014-07-29 22:50:48 +0300

    added parseMessage and Ping capabilities

--- main.rb
+++ main.rb
@@ -9,7 +9,44 @@ connection.puts "NICK #{config['nickname']}"
 connection.puts "JOIN #{config['channel']}"
 connection.puts "PRIVMSG #{config['channel']} :Hello world!"
 
+def sendPong(connection, server)
+  connection.puts "PONG #{server}"
+end
+
+def sendMessage(connection, target, message)
+  connection.puts "PRIVMSG #{target} :#{message}"
+end
+
+def parseMessage(message)
+
+  if message.include? "PING"
+    return "PING"
+  else
+    words = message.split(" ")
+    sender = words[0]
+    ircMessageType = words[1]
+    target = words[2]
+
+    if /^:!(\w+)/.match words[3]
+      return words[3].tr('!','').upcase
+    end
+
+  end
+
+end
+
 while true do
   msg = connection.gets
+
+  msgType = parseMessage(msg)
+
+  if msgType == "PING"
+    sendPong(connection, config['server'])
+    puts "PONG message sent"
+  else
+    puts msgType
+  end
+
   puts msg
+
 end
